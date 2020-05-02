5f13458bc6d4a2f52a9d40794f54ba67f415da2a
atriple <andiaqil.dpe@gmail.com>
2020-05-02 07:05:43 +0700

    Add generate model function & recycler view

--- app/src/main/java/com/atriple/study/celestial_objects/MainActivity.kt
+++ app/src/main/java/com/atriple/study/celestial_objects/MainActivity.kt
@@ -2,11 +2,19 @@ package com.atriple.study.celestial_objects
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.atriple.study.celestial_objects.model.CelestialObject
+import com.atriple.study.celestial_objects.model.generate
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvCelestialObject : RecyclerView
+    private val listOfCelestialObject : List<CelestialObject> = generate()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvCelestialObject = findViewById(R.id.rv_celestial_objects)
+        rvCelestialObject.setHasFixedSize(true)
     }
 }
