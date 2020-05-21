a346285ef0883c04481ba1b4e24ff0e4aa3ae59c
atriple <andiaqil.dpe@gmail.com>
2020-05-22 05:04:23 +0700

    Add custom menu bar

--- app/src/main/java/com/atriple/study/celestial_objects/MainActivity.kt
+++ app/src/main/java/com/atriple/study/celestial_objects/MainActivity.kt
@@ -2,6 +2,8 @@ package com.atriple.study.celestial_objects
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.atriple.study.celestial_objects.adapter.ListCelestialObjectAdapter
@@ -9,12 +11,13 @@ import com.atriple.study.celestial_objects.model.CelestialObject
 import com.atriple.study.celestial_objects.model.generate
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var rvCelestialObject : RecyclerView
-    private val listOfCelestialObject : List<CelestialObject> = generate()
+    private lateinit var rvCelestialObject: RecyclerView
+    private val listOfCelestialObject: List<CelestialObject> = generate()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.main_toolbar))
 
         rvCelestialObject = findViewById(R.id.rv_celestial_objects)
         rvCelestialObject.setHasFixedSize(true)
@@ -22,5 +25,20 @@ class MainActivity : AppCompatActivity() {
         rvCelestialObject.layoutManager = LinearLayoutManager(this)
         rvCelestialObject.adapter = ListCelestialObjectAdapter(listOfCelestialObject)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_about -> {
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
